15d01d7c140191724aad47f75e66f5391f9dfec6
Jeffry Gonzalez <jeff@hypertheory.com>
2020-10-14 15:43:24 -0400

    Add project files.

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Net.Client;
+using GrpcServer;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Calling a GRPC Service");
+            Console.Write("Hit enter to do the deed");
+            Console.ReadLine();
+            using var channel = GrpcChannel.ForAddress("https://localhost:50001");
+            var client = new Greeter.GreeterClient(channel);
+
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = "Putintane" });
+
+            Console.WriteLine($"Got the response {reply.Message}");
+
+        }
+    }
+}
